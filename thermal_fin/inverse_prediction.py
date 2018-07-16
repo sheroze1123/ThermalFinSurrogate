2fc6e1c1716efced9df2f2ab59006d83c9c7b3a1
sheroze1123 <mss385@cornell.edu>
2018-07-16 14:02:00 -0500

    Forward solver ready for CNN

--- thermal_fin/inverse_prediction.py
+++ thermal_fin/inverse_prediction.py
@@ -32,7 +32,7 @@ class ForwardSolver:
         x = np.linspace(-3.0, 3.0, grid_x)
         y = np.linspace(0.0, 4.0, grid_y)
         self.xx, self.yy = np.meshgrid(x, y)
-        self.Aq_s, self.Fh, self.nodes, self.coor, self.theta_tri = load_FEM()
+        self.Aq_s, self.Fh, self.nodes, self.coor, self.theta_tri = self.load_FEM()
 
     def solve(self, params):
         '''
@@ -64,43 +64,40 @@ class ForwardSolver:
 
         return uh_interpolated
 
+    def load_FEM(self):
+        '''
+        Loads the FEM matrices in sparse format for the forward solve
+        Only 6 RHS matrices corresponding to the parameters loaded currently.
+        Data generated from MATLAB, so the indices need to be subtracted by 1.
+
+        Returns:
+            Aq_s : Array of Aq sparse matrices each with dimension (nodes,nodes)
+            Fh   : Load vector for FEM with dimension (nodes,1)
+            nodes: Number of FEM nodes 
+            coor: coordinates for plottting
+            theta_tri: coordinate indices for the triangulation with dimension (triangles, 3)
+        '''
+
+        Aq_s = []
+
+        data_dir = 'matlab_data/'
+        Fh = np.loadtxt(data_dir + 'Fh.csv')
+        nodes = Fh.shape[0]
+        coor = np.loadtxt(data_dir + 'coarse_coor.csv', delimiter=',')
+        theta_tri = np.loadtxt(data_dir + 'theta_tri.csv',
+                               delimiter=",", unpack=True)
+        for i in range(1, 7):
+            col, row, value = np.loadtxt(
+                data_dir + 'Aq' + str(i) + '.csv', delimiter="\t", unpack=True)
+            Aq = coo_matrix((value, (row-1, col-1)), shape=(nodes, nodes))
+            Aq_s.append(Aq)
+
+        return Aq_s, Fh, nodes, coor, (theta_tri-1).T
+
     def plot_solution(self, uh_interpolated, filepath):
-        pl = plt.pcolormesh(self.xx, self.yy, uh_interpolated,linewidth=0, rasterized=True)
+        pl = plt.pcolormesh(self.xx, self.yy, uh_interpolated, linewidth=0.0, rasterized=True)
         pl.set_edgecolor('face')
         plt.savefig(filepath, dpi=400)
-
-
-
-def load_FEM():
-    '''
-    Loads the FEM matrices in sparse format for the forward solve
-    Only 6 RHS matrices corresponding to the parameters loaded currently.
-    Data generated from MATLAB, so the indices need to be subtracted by 1.
-
-    Returns:
-        Aq_s : Array of Aq sparse matrices each with dimension (nodes,nodes)
-        Fh   : Load vector for FEM with dimension (nodes,1)
-        nodes: Number of FEM nodes 
-        coor: coordinates for plottting
-        theta_tri: coordinate indices for the triangulation with dimension (triangles, 3)
-    '''
-
-    Aq_s = []
-
-    data_dir = 'matlab_data/'
-    Fh = np.loadtxt(data_dir + 'Fh.csv')
-    nodes = Fh.shape[0]
-    coor = np.loadtxt(data_dir + 'coarse_coor.csv', delimiter=',')
-    theta_tri = np.loadtxt(data_dir + 'theta_tri.csv',
-                           delimiter=",", unpack=True)
-    for i in range(1, 7):
-        col, row, value = np.loadtxt(
-            data_dir + 'Aq' + str(i) + '.csv', delimiter="\t", unpack=True)
-        Aq = coo_matrix((value, (row-1, col-1)), shape=(nodes, nodes))
-        Aq_s.append(Aq)
-
-    return Aq_s, Fh, nodes, coor, (theta_tri-1).T
-
 
 def cnn_model(features, labels, mode, params):
     '''
